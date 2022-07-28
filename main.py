5c9b980fd76204ec183fb749fd5ebb55023a4645
Victoria Ekefre <40358115+Enemini@users.noreply.github.com>
2022-07-28 04:04:01 +0000

    Version 1.0

--- main.py
+++ main.py
@@ -26,4 +26,27 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+import random 
 
+player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors."))
+if player_choice == 0:
+  print(rock)
+elif player_choice == 1:
+  print(paper)
+else:
+  print(scissors)
+  
+computer_choice = random.randint(0,2)
+if computer_choice == 0:
+  print(rock)
+elif computer_choice == 1:
+  print(paper)
+else:
+  print(scissors)
+  
+if player_choice == computer_choice:
+  print("It's a draw!")
+elif (player_choice == 1 and computer_choice == 0) or (player_choice == 2 and computer_choice == 1) or (player_choice == 0 and computer_choice == 2):
+  print("Player won!")
+else:
+  print("Computer won!")
